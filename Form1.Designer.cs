4fef6e8579cb0823b21ce70c32b9d71c521ecb64
ciokos <Ciokos23>
2018-06-11 22:36:45 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Snake
+{
+    partial class window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.food = new System.Windows.Forms.Panel();
+            this.head = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.table.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.AutoSize = true;
+            this.table.ColumnCount = 20;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.Controls.Add(this.food, 0, 0);
+            this.table.Controls.Add(this.head, 16, 8);
+            this.table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.AddColumns;
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Name = "table";
+            this.table.RowCount = 20;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table.Size = new System.Drawing.Size(400, 400);
+            this.table.TabIndex = 0;
+            // 
+            // food
+            // 
+            this.food.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.food.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.food.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.food.Location = new System.Drawing.Point(1, 1);
+            this.food.Margin = new System.Windows.Forms.Padding(1);
+            this.food.Name = "food";
+            this.food.Size = new System.Drawing.Size(18, 18);
+            this.food.TabIndex = 1;
+            // 
+            // head
+            // 
+            this.head.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.head.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.head.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.head.Location = new System.Drawing.Point(321, 161);
+            this.head.Margin = new System.Windows.Forms.Padding(1);
+            this.head.Name = "head";
+            this.head.Size = new System.Drawing.Size(18, 18);
+            this.head.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.table);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(408, 427);
+            this.MinimumSize = new System.Drawing.Size(408, 427);
+            this.Name = "window";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.window_KeyDown);
+            this.table.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel table;
+        private System.Windows.Forms.Panel head;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel food;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class window : Form
+    {
+        List<Panel> tail = new List<Panel>();
+        int xspeed = 1;
+        int yspeed = 0;
+        public window()
+        {
+            InitializeComponent();
+            resetFood();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            updateGame();
+        }
+
+        private void updateGame()
+        {
+            if (checkIfEnd())
+                endGame();
+            tryEat();
+            moveSnake();
+        }
+
+        private void endGame()
+        {
+            foreach (Panel p in tail)
+                p.Dispose();
+            tail.Clear();
+            timer1.Interval = 100;
+        }
+
+        private bool checkIfEnd()
+        {
+            foreach(Panel p in tail)
+            {
+                if(headoverlapping(p))
+                {
+                    return true;
+                }
+            }
+            if (tail.Count == table.RowCount * table.ColumnCount - 1)
+                return true;
+            return false;
+        }
+
+        private void resetFood()
+        {
+            food.Hide();
+            Random rnd = new Random();
+            int x = rnd.Next(0, table.ColumnCount);
+            int y = rnd.Next(0, table.RowCount);
+            while (!isOk(x, y))
+            {
+                x = rnd.Next(0, table.ColumnCount);
+                y = rnd.Next(0, table.RowCount);
+            }
+            table.SetCellPosition(food, new TableLayoutPanelCellPosition(x, y));
+            food.Show();
+        }
+
+        private bool isOk(int x, int y)
+        {
+            foreach(Panel p in tail)
+            {
+                if (table.GetCellPosition(p) == new TableLayoutPanelCellPosition(x, y))
+                    return false;
+            }
+            if (table.GetCellPosition(head) == new TableLayoutPanelCellPosition(x, y))
+                return false;
+            if (table.GetCellPosition(food) == new TableLayoutPanelCellPosition(x, y))
+                return false;
+            return true;
+        }
+        private void tryEat()
+        {
+            if(headoverlapping(food))
+            {
+                Panel extend = new Panel();
+                extend.BackColor = head.BackColor;
+                extend.Size = head.Size;
+                extend.Margin = head.Margin;
+                table.Controls.Add(extend);
+                extend.Dock = DockStyle.Fill;
+                tail.Add(extend);
+                extend.Hide();
+                timer1.Interval += timer1.Interval / 100 * (-1);
+            }
+        }
+
+        private void moveSnake()
+        {
+            int x = table.GetCellPosition(head).Column;
+            int y = table.GetCellPosition(head).Row;
+            movehead(xspeed, yspeed);
+            movetail(x, y);
+        }
+        
+        private void movehead(int x, int y)
+        {
+            int col = (table.GetCellPosition(head).Column + x) % table.ColumnCount;
+            int row = (table.GetCellPosition(head).Row + y) % table.RowCount;
+            if (table.GetCellPosition(head).Column == 0 && xspeed == -1)
+            {
+                col = table.ColumnCount-1;
+            }
+            if (table.GetCellPosition(head).Row == 0 && yspeed == -1)
+            {
+                row = table.RowCount-1;
+            }
+
+            table.SetCellPosition(head, new TableLayoutPanelCellPosition(col, row));
+        }
+
+        private void movetail(int x, int y)
+        {
+            if (tail.Count > 0)
+            {
+                table.SetCellPosition(tail[tail.Count - 1], new TableLayoutPanelCellPosition(x, y));
+                Panel move = tail.Last();
+                tail.Insert(0, move);
+                tail[0].Show();
+                tail.RemoveAt(tail.Count - 1);
+            }
+        }
+
+        private int panelcol(Panel p)
+        {
+            return table.GetCellPosition(p).Column;
+        }
+
+        private int panelrow(Panel p)
+        {
+            return table.GetCellPosition(p).Row;
+        }
+
+        private bool headoverlapping(Panel p)
+        {
+            if (table.GetCellPosition(p) == new TableLayoutPanelCellPosition(panelcol(head) + xspeed, panelrow(head) + yspeed))
+            {
+                resetFood();
+                return true;
+            }
+                
+            return false;
+        }
+
+        private void window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up && (yspeed!=1 || tail.Count==0))
+            {
+                xspeed = 0;
+                yspeed = -1;
+            }
+            else if(e.KeyCode == Keys.Down && (yspeed != -1 || tail.Count == 0))
+            {
+                xspeed = 0;
+                yspeed = 1;
+            }
+            else if (e.KeyCode == Keys.Right && (xspeed != -1 || tail.Count == 0))
+            {
+                xspeed = 1;
+                yspeed = 0;
+            }
+            else if (e.KeyCode == Keys.Left && (xspeed != 1 || tail.Count == 0))
+            {
+                xspeed = -1;
+                yspeed = 0;
+            }
+        }
+    }
+}
